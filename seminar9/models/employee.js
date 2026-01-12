2611a2452f20d1c01a9796181a980e0f60be3ec3
mariax0 <mariamirabelat@gmail.com>
2026-01-12 03:05:32 +0200

    Seminar 9

---
+++ seminar9/models/employee.js
@@ -0,0 +1,40 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../sequelize");
+
+const Employee = sequelize.define("Employee", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  firstName: DataTypes.STRING,
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      len: {
+        args: [3, 10],
+        msg: "Please enter 3-10 characters.",
+      },
+    },
+  },
+  email: {
+    type: DataTypes.STRING,
+    validate: {
+      isEmail: true,
+    },
+  },
+  birthYear: {
+    type: DataTypes.INTEGER,
+    validate: {
+      min: 1900,
+    },
+  },
+  salary: {
+    type: DataTypes.FLOAT,
+    defaultValue: 0,
+    validate: {
+      min: 0,
+    },
+  },
+});
